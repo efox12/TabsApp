bfce15cc95b05396f21bf61d74c81ea61e95aabf
efox12 <erikfox@Eriks-MacBook-Pro.local>
2017-11-28 16:42:19 -0800

    Added some more stuff

--- Tabs/app/src/main/java/com/foxbrajcich/tabs/GroupActivity.java
+++ Tabs/app/src/main/java/com/foxbrajcich/tabs/GroupActivity.java
@@ -121,7 +121,7 @@ public class GroupActivity extends AppCompatActivity {
         if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
             if(data.hasExtra("expense")){
                 expenses.add((Expense) data.getSerializableExtra("expense"));
-                dbHelper.addExpenseToGroup(group, (Expense) data.getSerializableExtra("expense"));
+                dbHelper.addExpenseToGroup(group,(Expense) data.getSerializableExtra("expense"));
                 adapter.notifyDataSetChanged();
             }
         }
