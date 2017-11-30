3223f2a58530664700f7e9aef790ded28df45ba7
Robert Brajcich <Rbrajcich@comcast.net>
2017-11-29 21:48:44 -0800

    added gradle dependencies and google-services.json which are required for firebase use

--- Tabs/app/src/main/java/com/foxbrajcich/tabs/GroupInfoActivity.java
+++ Tabs/app/src/main/java/com/foxbrajcich/tabs/GroupInfoActivity.java
@@ -9,7 +9,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
